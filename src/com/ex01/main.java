850bffdd3fb865d802ec2db3de348e4a76c287cb
UASN128 <nico.buttgereit@stud.fra-uas.de>
2026-04-30 01:09:46 +0200

    Completed task 02

---
+++ src/com/ex01/main.java
@@ -0,0 +1,10 @@
+package com.ex01;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("Starting Task");
+		task02.main(args);
+		System.out.println("Task completed");
+	}
+
+}
--- src/com/ex01/task02.java
+++ src/com/ex01/task02.java
@@ -1,5 +1,19 @@
 package com.ex01;
 
 public class task02 {
-
+	public static int main(String[] args) {
+		testing_ulam();
+		return 0;
+	}
+	public static void ulam(int n) {
+		while(n > 1) {
+			if(n % 2 == 0) n /= 2;
+			else n = (n * 3) + 1;
+		}
+	}
+	public static void testing_ulam() {
+		for(int i = 1; i < 1000000; i++) {
+			ulam(i);
+		}
+	}
 }
